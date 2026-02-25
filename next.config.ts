a6f59e1377bb18e9e90d6ec420501c57b311a0bf
Infrastructure Baalvion <infra.baalvion@gmail.com>
2026-02-25 10:13:39 +0000

    https://s24.q4cdn.com/856567660/files/design/latest-quarter.jpg remove t

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 's24.q4cdn.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
